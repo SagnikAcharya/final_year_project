03c9d82d55402d0b8a96ccaea1bdc23f7add74d9
SagnikAcharya <sagnikacharya17@gmail.com>
2024-05-30 00:15:31 +0530

    updated mongostore

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 ///////////////////////////////////////////////////   MONGODB/DATABASE CONNECTION     ///////////////////////////////////////////////
-const connection=mongoose.connect(dbURL);
+mongoose.connect(dbURL);
+const connection=mongoose.connection;
 connection.on("error", console.error.bind(console, "connection error"));
 connection.once("open", () => {
   console.log("Database Connected");
